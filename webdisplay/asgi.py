302cbd16761c32f364cdc98f23faf388a83c89dc
Your Name <you@example.com>
2024-01-30 15:10:00 +0530

    created project

---
+++ webdisplay/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for webdisplay project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webdisplay.settings')
+
+application = get_asgi_application()
